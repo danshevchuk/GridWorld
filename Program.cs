50b86d55c82a4f46310fadfef59dad99244bd4f9
danshevchuk <dan@strawbfields.com>
2023-10-06 18:29:54 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GridWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            var grid = InitGrid("Maps/map01.txt");
+            var maxMoves = GameParameters.MaxMoves;
+            PlayerLoop(grid, maxMoves);
+        }
+
+        private static void PlayerLoop(Grid grid, int maxMoves)
+        {
+            int moves = 0;
+            while (moves < maxMoves)
+            {
+                var goalAchieved = grid.Update();
+                if(goalAchieved)
+                {
+                    Console.WriteLine("Goal Reached!");
+                    break;
+                }
+                moves++;
+
+                Thread.Sleep(50);
+            }
+
+            if (moves >= maxMoves)
+            {
+                Console.WriteLine("Moves exhausted. Try again.");
+            }
+        }
+
+        private static Grid InitGrid(string mapPath)
+        {
+            var grid = new Grid();
+
+            grid.LoadGridFromFile(mapPath);
+            grid.DisplayGrid();
+
+            return grid;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This app is a .NET 7 console application
+
+To run the app, run "dotnet run" in the terminal 
